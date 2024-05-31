ccdc3bfa0d3505160bb0babd2a19c60fc2c38087
James M. Irving, Ph.D. <james.irving.phd@gmail.com>
2024-05-31 19:25:15 -0400

    updated evaluation functions to handle large # of classes

--- ann_functions.py
+++ ann_functions.py
@@ -194,7 +194,8 @@ def evaluate_classification_network(model,
                                     cmap_test="Reds",
                                     values_format=".2f", 
                                     colorbar=False, target_names=None, 
-         return_fig=False):
+         return_fig=False, as_frame=False, frame_include_support = True, 
+         frame_include_macro_avg=True):
     """Evaluates a neural network classification task using either
     separate X and y arrays or a tensorflow Dataset
 
@@ -226,19 +227,25 @@ def evaluate_classification_network(model,
     """
     if (X_train is None) & (X_test is None):
         raise Exception('\nEither X_train & y_train or X_test & y_test must be provided.')
- 
+    # Initialize dict
+    results_dict = {}
+    
+    # Shared Kwargs
     shared_kwargs = dict(output_dict=True, 
                       figsize=figsize,
                       colorbar=colorbar,
                       values_format=values_format, 
                       target_names=target_names,)
+    
     # Plot history, if provided
     if history is not None:
         plot_history(history, figsize=history_figsize)
+        
     ## Adding a Print Header
     print("\n"+'='*80)
     print('- Evaluating Network...')
     print('='*80)
+    
     ## TRAINING DATA EVALUATION
     # check if X_train was provided
     if X_train is not None:
@@ -262,8 +269,9 @@ def evaluate_classification_network(model,
         print(model.evaluate(X_train, return_dict=True))
     
     # If no X_train, then save empty list for results_train
-    else:
-        results_train = None
+        results_dict['train'] = results_train
+    # else:
+    #     results_train = None
   
   
     ## TEST DATA EVALUATION
@@ -287,15 +295,20 @@ def evaluate_classification_network(model,
         ## Run model.evaluate         
         print("\n- Evaluating Test Data:")
         print(model.evaluate(X_test, return_dict=True))
-      
+        
+        # Add results to dict
+        results_dict['test'] = results_test
     # If no X_test, then save empty list for results_test
-    else:
-        results_test = None
+    # else:
+    #     results_test = None
       
     if (output_dict == True) | (return_fig==True):
-        # Store results in a dataframe if ouput_frame is True
-        results_dict = {'train':results_train,
-                        'test': results_test}
+        # # Store results in a dataframe if ouput_frame is True
+        # results_dict = {'train':results_train,
+        #                 'test': results_test}
+        if as_frame:
+            results_dict = {k: get_results_df(results_dict, results_key=k,include_support=frame_include_support,
+                                              include_macro_avg=frame_include_macro_avg) for k in results_dict.keys()}
         return results_dict
 
 
@@ -461,102 +474,53 @@ def convert_y_to_sklearn_classes(y, verbose=False):
         return np.round(y).flatten().astype(int)
 
 
-# def evaluate_classification_network(model, 
-#                                     X_train=None, y_train=None, 
-#                                     X_test=None, y_test=None,
-#                                     history=None, history_figsize=(6,6),
-#                                     figsize=(6,4), normalize='true',
-#                                     output_dict = False,
-#                                     cmap_train='Blues',
-#                                     cmap_test="Reds",
-#                                     values_format=".2f", 
-#                                     colorbar=False):
-#     """Evaluates a neural network classification task using either
-#     separate X and y arrays or a tensorflow Dataset
+
+
+
+
+def get_results_df(results_dict, results_key='test', 
+                   average_rowname= 'macro avg',
+                   include_support = True,
+                   include_macro_avg=True):
+    """
+    Convert a results dictionary into a pandas DataFrame.
+
+    Parameters:
+    - results_dict (dict): A dictionary containing the results.
+    - results_key (str): The key in the dictionary that contains the results. Default is 'test'.
+    - average_rowname (str): The name of the row that represents the average. Default is 'macro avg'.
+    - include_support (bool): Whether to include the 'support' column in the DataFrame. Default is True.
+    - include_macro_avg (bool): Whether to include the 'macro avg' row in the DataFrame. Default is True.
+
+    Returns:
+    - results_df (pandas DataFrame): A DataFrame containing the results.
+
+    """
+    import pandas as pd
+    results = results_dict[results_key].copy()
     
-#     Data Args:
-#         X_train (array, or Dataset)
-#         y_train (array, or None if using a Dataset
-#         X_test (array, or Dataset)
-#         y_test (array, or None if using a Dataset)
-#         history (history object)
-#         """
-#     # Plot history, if provided
-#     if history is not None:
-#         plot_history(history, figsize=history_figsize)
-
-#     ## Adding a Print Header
-#     print("\n"+'='*80)
-#     print('- Evaluating Network...')
-#     print('='*80)
-
+    # Remove accuracy and macro avg from results
+    accuracy = results.pop('accuracy')
+    macro_avg = results.pop('macro avg')
+    _ = results.pop('weighted avg')
     
-#     ## TRAINING DATA EVALUATION
-#     # check if X_train was provided
-#     if X_train is not None:
-#         ## Check if X_train is a dataset
-#         if hasattr(X_train,'map'):
-#             # If it IS a Datset:
-#             # extract y_train and y_train_pred with helper function
-#             y_train, y_train_pred = get_true_pred_labels(model, X_train)
-#         else:
-#             # Get predictions for training data
-#             y_train_pred = model.predict(X_train)
-
-#         ## Pass both y-vars through helper compatibility function
-#         y_train = convert_y_to_sklearn_classes(y_train)
-#         y_train_pred = convert_y_to_sklearn_classes(y_train_pred)
-        
-#         # Call the helper function to obtain regression metrics for training data
-#         results_train = classification_metrics(y_train, y_train_pred, 
-#                                          output_dict=True, figsize=figsize,
-#                                              colorbar=colorbar, cmap=cmap_train,
-#                                                values_format=values_format,
-#                                          label='Training Data')
-        
-#         ## Run model.evaluate         
-#         print("\n- Evaluating Training Data:")
-#         print(model.evaluate(X_train, return_dict=True))
+    # Create DataFrames
+    results_df = pd.DataFrame(results).T
     
-#     # If no X_train, then save empty list for results_train
-#     else:
-#         results_train = []
+    if include_macro_avg:
+        overall_results = pd.DataFrame(macro_avg, index=[average_rowname])#.T
+    
+        ## Concatenate the overall results to the results_df
+        results_df = pd.concat([results_df, overall_results],axis=0)
+        results_df.loc[average_rowname,'accuracy'] = accuracy
+    
+    # Recast support as int
+    results_df['support'] = results_df['support'].astype(int)
 
-
-#     ## TEST DATA EVALUATION
-#     # check if X_test was provided
-#     if X_test is not None:
-#         ## Check if X_train is a dataset
-#         if hasattr(X_test,'map'):
-#             # If it IS a Datset:
-#             # extract y_train and y_train_pred with helper function
-#             y_test, y_test_pred = get_true_pred_labels(model, X_test)
-#         else:
-#             # Get predictions for training data
-#             y_test_pred = model.predict(X_test)
-
-#         ## Pass both y-vars through helper compatibility function
-#         y_test = convert_y_to_sklearn_classes(y_test)
-#         y_test_pred = convert_y_to_sklearn_classes(y_test_pred)
-        
-#         # Call the helper function to obtain regression metrics for training data
-#         results_test = classification_metrics(y_test, y_test_pred, 
-#                                          output_dict=True, figsize=figsize,
-#                                              colorbar=colorbar, cmap=cmap_test,
-#                                               values_format=values_format,
-#                                          label='Test Data')
-        
-#         ## Run model.evaluate         
-#         print("\n- Evaluating Test Data:")
-#         print(model.evaluate(X_test, return_dict=True))
-      
-#     # If no X_test, then save empty list for results_test
-#     else:
-#         results_test = []
-      
-
-#     # Store results in a dataframe if ouput_frame is True
-#     results_dict = {'train':results_train,
-#                     'test': results_test}
-#     return results_dict
-
+    # Move the support column to the end
+    # results_df = results_df[ results_df.drop(columns='support').columns.tolist() + ['support']]
+    
+    if not include_support:
+        results_df = results_df.drop(columns='support')
+    
+    return results_df
